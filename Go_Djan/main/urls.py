e3cc05d0acc9d0b9fa08c5108babbcb815b18584
UrokiVV <vvsoft@yahoo.com>
2025-09-05 12:01:59 +0300

    commit N2

--- Go_Djan/main/urls.py
+++ Go_Djan/main/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('newtest', views.newtest),
-    path('data', views.data)
+    # DJ02
+    path('', views.index, name='index'),
+    path('newtest', views.newtest, name='newtest'),
+    path('data', views.data, name='data'),
+    path('next04', views.next04, name='next04'),
+    path('about', views.about, name='about'), # footer
+    # DJ01
+    path('index01', views.index01),
+    path('new', views.new),
+    path('newtest01', views.newtest01),
+    path('data01', views.data01)
 ]
--- Go_Djan/main/views.py
+++ Go_Djan/main/views.py
@@ -1,25 +1,49 @@
+# main\views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):  # главная стр
+    return render(request, 'main/index.html')
 
-def index(request):
+
+def newtest(request):   # вторая стр
+    return render(request, 'main/newtest.html')
+
+
+def data(request):     # третья стр
+    return render(request, 'main/data.html')
+
+
+def next04(request):     # четвёртая стр
+    return render(request, 'main/next04.html')
+
+
+def about(request):     # Подвал
+    return render(request, 'main/footer.html')
+
+
+def new(request):
+    return render(request, 'main/new.html')
+
+
+def index01(request):
     s1 = "<h1>Это новый проект Go_Djan</h1>"
     s2 = "<h2>Проект Go_Djan разработан на Django</h2>"
-    s3 = "<h3>Это - только начало </h3>"
+    s3 = "<h3>Страница 'index01' - это только начало </h3>"
     return HttpResponse(s1 + s2 + s3)
 
 
-def newtest(request):
-    s1 = "<h1>Вторая страница 'newtest' проекта Go_Djan</h1>"
+def newtest01(request):
+    s1 = "<h1>Вторая страница 'newtest01' проекта Go_Djan</h1>"
     s2 = "<h2>Проект Go_Djan разработан на Django</h2>"
     s3 = "<h3>Проба пера... Проба... </h3>"
     return HttpResponse(s1 + s2 + s3)
 
 
-def data(request):
-    s1 = "<h1>Третья страница 'data' проекта Go_Djan</h1>"
+def data01(request):
+    s1 = "<h1>Третья страница 'data01' проекта Go_Djan</h1>"
     s2 = "<h2>Проект Go_Djan разработан на Django</h2>"
     s3 = "<h3> и так далее...</h3>"
     return HttpResponse(s1 + s2 + s3)
